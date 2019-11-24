a39064ec321d431231a777b5e395b311d60c3429
Alex Poulos <matpoulos@gmail.com>
2019-11-23 21:01:03 -0500

    Resolves TMP-8. Auth now working.

--- update-lambda.sh
+++ update-lambda.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 zip tempora.zip -r index.js src/ node_modules/
 aws lambda update-function-code --function-name tempora-auth --zip-file fileb://tempora.zip
+aws lambda update-function-code --function-name tempora-api-authorizer --zip-file fileb://tempora.zip
